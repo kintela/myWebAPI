e74616d848e5419c4fa2e6c306baf9433a3edae9
kintela <rquintela@fulcrum.es>
2024-02-07 17:27:40 +0100

    Creacion inicial del proyecto

---
+++ KintelaApp/KintelaDomain/Plato.cs
@@ -0,0 +1,11 @@
+﻿namespace KintelaDomain
+{
+	public class Plato
+	{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string[] Categorias { get; set; }
+		public string Imagen { get; set; }
+
+	}
+}
---
+++ KintelaApp/KintelaDomain/Receta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintelaDomain
+{
+	public class Receta
+	{
+		public int Id { get; set; }
+    public string[] Ingredientes { get; set; }
+		public string[] Preparacion { get; set; }
+		public string[] Presentacion { get; set; }
+		public int Comensales { get; set; }
+		public string enlaceVideo { get; set; }		
+		Plato Plato { get; set; }
+    public int PlatoId { get; set; }
+   }
+}
