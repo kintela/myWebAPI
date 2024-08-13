8b06a4ea5da4c7fb4aa15e0ba3a7296868b973fc
kintela <rquintela@fulcrum.es>
2024-08-13 18:00:09 +0200

    Recetas End point

---
+++ KintelaApp/KintelaAPI/RecetasEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KintelaData;
+using KintelaDomain;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+namespace KintelaAPI;
+
+public static class RecetasEndpoints
+{
+    public static void MapRecetasEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Recetas").WithTags(nameof(Categoria));
+
+        group.MapGet("/Categorias", async (KintelaContext db) =>
+        {
+						return await db.Categorias.OrderBy(c=>c.Nombre).ToListAsync();
+				})
+        .WithName("GetAllCategorias")
+        .WithOpenApi();
+
+        
+    }
+}
